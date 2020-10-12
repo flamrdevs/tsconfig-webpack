5e19dde797110ce0133350080336481f4ac69297
flamrdevs <flamrdevs@gmail.com>
2020-10-12 12:43:03 +0700

    update project

---
+++ tsconfig-webpack.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const tsConfigWebpack = ({ tsConfigPath = './tsconfig.json', webpackConfigBasePath = __dirname } = {}) => {
+  // get paths from destructuring tsconfig.json
+  const { paths } = require(tsConfigPath).compilerOptions;
+
+  // ts alias (paths)
+  const ts_alias = {};
+
+  for (const [_alias,_path] of Object.entries(paths)) {
+    // ts_alias[new_alias] = new_path
+    ts_alias[_alias.replace('/*','')] = path.resolve(webpackConfigBasePath,_path[0].replace('/*','').replace('*',''));
+  }
+
+  // return ts alias to webpack alias
+  return ts_alias;
+}
+
+module.exports = tsConfigWebpack;
